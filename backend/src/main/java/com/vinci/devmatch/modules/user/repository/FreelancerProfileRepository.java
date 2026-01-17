6bfb72097773f07a341ebe075efb7689d406e858
Vencent Delute <iceiceicebell@gmail.com>
2026-01-17 15:22:25 +0800

    refactor user repositories to use Auth0 ID and add existence checks

--- backend/src/main/java/com/vinci/devmatch/modules/user/repository/FreelancerProfileRepository.java
+++ backend/src/main/java/com/vinci/devmatch/modules/user/repository/FreelancerProfileRepository.java
@@ -2,10 +2,14 @@ package com.vinci.devmatch.modules.user.repository;
 
 import com.vinci.devmatch.modules.user.entity.FreelancerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FreelancerProfileRepository extends JpaRepository<FreelancerProfile, Long> {
 
     Optional<FreelancerProfile> findByUserId(Long userId);
+
+    boolean existsByUserId(Long userId);
 }
